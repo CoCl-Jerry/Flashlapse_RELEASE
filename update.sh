1468ec5473e6ecafc3637589f50d8a6fba01b170
CoCl-Jerry <jerry@cosecloud.com>
2021-05-07 04:54:02 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
+sudo rm -r /home/pi/Documents/Flashlapse_RELEASE
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-	sudo rm -r /home/pi/Documents/Flashlapse_RELEASE
-	cd /home/pi/Documents
-	git clone -b MO63130 https://github.com/CoCl-Jerry/Flashlapse_RELEASE.git
+	cd /home/pi/Documents/backup/Flashlapse_RELEASE
+	git pull
 fi
+sudo cp -r /home/pi/Documents/backup/Flashlapse_RELEASE /home/pi/Documents
